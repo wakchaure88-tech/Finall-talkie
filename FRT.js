942b23b3b2dcd6333b1ea1320a353a9612762832
Sarthak Wakchaure <sarthakwakchaure88@gmail.com>
2022-02-16 11:50:57 +0530

    update commit

--- FRT.js
+++ FRT.js
@@ -1,38 +1,58 @@
 
-        var txtInput = document.querySelector('#input');
-        var voiceList = document.querySelector('#voiceList');
-        var speak= document.querySelector('#speak');
-        var synth = window.speechSynthesis;
-        var voices = [];
+    var txtInput = document.querySelector('#input');
+    var voiceList = document.querySelector('#voiceList');
+    var speak= document.querySelector('#speak');
+    var pause= document.querySelector('#pause');
+    var resume= document.querySelector('#resume');
+    var synth = window.speechSynthesis;
+    var voices = [];
 
-        PopulateVoices();
-        if(speechSynthesis !== undefined){
-            speechSynthesis.onvoiceschanged = PopulateVoices;
-        }
+    PopulateVoices();
+    if(speechSynthesis !== undefined){
+        speechSynthesis.onvoiceschanged = PopulateVoices;
+    }
+         
+    //speak
+    speak.addEventListener('click', ()=> 
+    {
+        var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
+        var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+        voices.forEach((voice)=>
+        {
+            if(voice.name === selectedVoiceName)
+            {
+                toSpeak.voice = voice;
+            }
+        });
+        
+        synth.speak(toSpeak);
+    });
 
-        speak.addEventListener('click', ()=> {
-            var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
-            var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
-            voices.forEach((voice)=>{
-                if(voice.name === selectedVoiceName){
-                    toSpeak.voice = voice;
-                }
-            });
-            synth.speak(toSpeak);
+    function PopulateVoices()
+    {
+        voices = synth.getVoices();
+        var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+        voiceList.innerHTML = '';
+        voices.forEach((voice)=>
+        {
+            var listItem = document.createElement('option');
+            listItem.textContent = voice.name;
+            listItem.setAttribute('data-lang', voice.lang);
+            listItem.setAttribute('data-name', voice.name);
+            voiceList.appendChild(listItem);
         });
 
-        function PopulateVoices(){
-            voices = synth.getVoices();
-            var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
-            voiceList.innerHTML = '';
-            voices.forEach((voice)=>{
-                var listItem = document.createElement('option');
-                listItem.textContent = voice.name;
-                listItem.setAttribute('data-lang', voice.lang);
-                listItem.setAttribute('data-name', voice.name);
-                voiceList.appendChild(listItem);
-            });
-
-            voiceList.selectedIndex = selectedIndex;
-        }
+        voiceList.selectedIndex = selectedIndex;
+    }
    
+    //PAUSE 
+    pause.addEventListener('click', ()=> 
+    {
+        window.speechSynthesis.pause(); 
+    }); 
+   
+    //RESUME 
+    resume.addEventListener('click', ()=> 
+    {
+        window.speechSynthesis.resume(); 
+    });
